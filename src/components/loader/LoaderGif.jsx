6526f7c9189d745434f03752bd1f3b049a5103a5
Roman Baranovskii <rom8614@gmail.com>
2024-09-20 22:23:42 +0300

    fixes main

--- src/components/loader/LoaderGif.jsx
+++ src/components/loader/LoaderGif.jsx
@@ -1,12 +1,47 @@
+// import Image from "next/image";
+//
+// import styles from './Loader.module.scss'
+//
+// const loader = '/loading.gif'
+// export const LoaderGif = () => {
+//     return(
+//         <div className={styles.root}>
+//             <Image className={styles.video} width={450} height={1000} src={loader} alt="Loading..."/>
+//         </div>
+//     )
+// }
+
+import {useEffect, useState} from "react";
 import Image from "next/image";
 
 import styles from './Loader.module.scss'
 
-const loader = '/loading.gif'
+const loader = '/loadingImg.jpg'
 export const LoaderGif = () => {
     return(
         <div className={styles.root}>
-            <Image className={styles.video} width={450} height={1000} src={loader} alt="Loading..."/>
+            <Image className={styles.video} src={loader} alt="Loading..."/>
+            <LoadingText />
         </div>
     )
 }
+
+const LoadingText = () => {
+    const [dots, setDots] = useState(0);
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+            setDots(prevDots => (prevDots + 1) % 4);
+        }, 500);
+
+        return () => clearInterval(interval);
+    }, []);
+
+    return (
+        <div className={styles.loading}>
+            Loading{'.'.repeat(dots)}
+        </div>
+    );
+};
+
+export default LoadingText;
