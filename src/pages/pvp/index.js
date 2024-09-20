5d55d159d5b05a46f3f362a4d19d4edb8d7a3990
Roman Baranovskii <rom8614@gmail.com>
2024-09-20 23:47:38 +0300

    fixes btns

--- src/pages/pvp/index.js
+++ src/pages/pvp/index.js
@@ -201,7 +201,7 @@ export default function PvpPage() {
 
     return (
         <>
-            {!isLoadingPvp ? (
+            {isLoadingPvp ? (
                 <LoaderGif />
             ) : (
                 <>
