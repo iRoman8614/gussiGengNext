4dcbc7b84ef705f486a39635686f0bfc1fdfc6e7
Roman Baranovskii <rom8614@gmail.com>
2024-09-21 19:52:17 +0300

    gif

--- src/components/loader/LoaderGif.jsx
+++ src/components/loader/LoaderGif.jsx
@@ -16,7 +16,7 @@ import Image from "next/image";
 
 import styles from './Loader.module.scss'
 
-const loader = '/loading.gif'
+const loader = '/loadingImg.jpg'
 export const LoaderGif = () => {
     return(
         <div className={styles.root}>
