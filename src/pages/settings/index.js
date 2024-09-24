d0b6f1fb62baf0960520a68f4884a66e320f0d8a
Roman Baranovskii <rom8614@gmail.com>
2024-09-24 10:23:06 +0300

    settings fixes

--- src/pages/settings/index.js
+++ src/pages/settings/index.js
@@ -1,10 +1,10 @@
 import React, {useEffect} from 'react';
 import Image from "next/image";
+import {useRouter} from "next/router";
 import {CustomSelect} from '@/components/selector/Select';
 import {ToggleSwitch} from "@/components/toggleswitch/ToggleSwitch";
 
 import styles from '@/styles/Settings.module.scss'
-import {useRouter} from "next/router";
 
 const bg = '/backgrounds/settingsBG.png'
 
