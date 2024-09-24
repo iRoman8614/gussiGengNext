af70cdaff3a5b4c0164cce4e1c653320ef1ee8cd
Roman Baranovskii <rom8614@gmail.com>
2024-09-24 19:06:41 +0300

    boards

--- src/components/ListItem/ListItem.jsx
+++ src/components/ListItem/ListItem.jsx
@@ -3,17 +3,20 @@ import Image from "next/image";
 
 export const ListItem = ({item, index}) => {
     return(
-        <div className={styles.root}>
-            <Image className={styles.bg} src={item.image} alt={''} width={450} height={65} />
-            <div className={styles.container}>
+        <>
+            <div className={styles.root}>
                 <div className={styles.avatar}>
-                    {item.avatar && <Image src={item.avatar} alt={'avatar'} width={60} height={60} /> }
+                    {item.avatar && <Image src={item.avatar} alt={'avatar'} width={40} height={44} /> }
                 </div>
-                <div className={styles.nickname}>{item.nickname}</div>
-                <div className={styles.sum}>{item.sum}</div>
-                {index && <div className={styles.index}>{index}</div>}
+                <div className={styles.container}>
+                    <Image className={styles.bg} src={item.image} alt={''} width={450} height={65} />
+                    <div className={styles.content}>
+                        <div className={styles.nickname}>{item.nickname}</div>
+                        <div className={styles.sum}>{item.sum}</div>
+                        {index && <div className={styles.index}>{index}</div>}
+                    </div>
+                </div>
             </div>
-
-        </div>
+        </>
     )
 }
--- src/pages/boards/index.js
+++ src/pages/boards/index.js
@@ -1,13 +1,20 @@
-import styles from '@/styles/Boards.module.scss'
+import React, {useEffect, useRef, useState} from "react";
 import Image from "next/image";
-import {ListItem} from "@/components/ListItem/ListItem";
-import React, {useEffect} from "react";
 import {useRouter} from "next/router";
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
+import 'swiper/css/navigation';
+import { Navigation, Controller } from 'swiper/modules';
+import {ListItem} from "@/components/ListItem/ListItem";
+
+import {ratingData} from '@/mock/ratingData'
+
+import styles from '@/styles/Boards.module.scss'
 
 const bg = '/backgrounds/leaderboardBG.png'
 
 export default function Page() {
-
+    const [activeIndex, setActiveIndex] = useState(0);
     const router = useRouter();
 
     useEffect(() => {
@@ -22,105 +29,83 @@ export default function Page() {
         }
     }, [router]);
 
-    const friends = [
-        {
-            nickname: 'Tupacshakur',
-            sum: '15M',
-            image: '/listItemsBG/1grbg.png'
-        },
-        {
-            nickname: 'Jhonnycash',
-            sum: '14.1M',
-            image: '/listItemsBG/3yfbg.png'
-        },
-        {
-            nickname: 'missyelliot',
-            sum: '70K',
-            image: '/listItemsBG/2bvbg.png'
-        },
-        {
-            nickname: 'missyelliot',
-            sum: '70K',
-            image: '/listItemsBG/3yfbg.png'
-        },
-        {
-            nickname: 'missyelliot',
-            sum: '70K',
-            image: '/listItemsBG/2bvbg.png'
-        },
-        {
-            nickname: 'missyelliot',
-            sum: '70K',
-            image: '/listItemsBG/4rrbg.png'
-        },
-        {
-            nickname: 'Tupacshakur',
-            sum: '15M',
-            image: '/listItemsBG/1grbg.png'
-        },
-        {
-            nickname: 'Jhonnycash',
-            sum: '14.1M',
-            image: '/listItemsBG/3yfbg.png'
-        },
-        {
-            nickname: 'missyelliot',
-            sum: '70K',
-            image: '/listItemsBG/2bvbg.png'
-        },
-        {
-            nickname: 'missyelliot',
-            sum: '70K',
-            image: '/listItemsBG/3yfbg.png'
-        },
-        {
-            nickname: 'missyelliot',
-            sum: '70K',
-            image: '/listItemsBG/2bvbg.png'
-        },
-        {
-            nickname: 'missyelliot',
-            sum: '70K',
-            image: '/listItemsBG/4rrbg.png'
-        },
-        {
-            nickname: 'Tupacshakur',
-            sum: '15M',
-            image: '/listItemsBG/1grbg.png'
-        },
-        {
-            nickname: 'Jhonnycash',
-            sum: '14.1M',
-            image: '/listItemsBG/3yfbg.png'
-        },
-        {
-            nickname: 'missyelliot',
-            sum: '70K',
-            image: '/listItemsBG/2bvbg.png'
-        },
-        {
-            nickname: 'missyelliot',
-            sum: '70K',
-            image: '/listItemsBG/3yfbg.png'
-        },
-        {
-            nickname: 'missyelliot',
-            sum: '70K',
-            image: '/listItemsBG/2bvbg.png'
-        },
-        {
-            nickname: 'missyelliot',
-            sum: '70K',
-            image: '/listItemsBG/4rrbg.png'
+    const characters = [
+        { name: 'Associate', icon: '/skins/bg.png' },
+        { name: 'Street soldier', icon: '/skins/bg2.png' },
+        { name: 'Hood hustler', icon: '/skins/bg3.png' },
+        { name: 'Block boss', icon: '/skins/bg4.png' },
+        { name: 'CApo', icon: '/skins/bg5.png' },
+        { name: 'Syndicate kingpin', icon: '/skins/bg3.png' },
+    ];
+
+    const swiperRef = useRef(null);
+    const handleSlidePrev = () => {
+        if (swiperRef.current) {
+            swiperRef.current.slidePrev();
         }
-    ]
+    };
+
+    const handleSlideNext = () => {
+        if (swiperRef.current) {
+            swiperRef.current.slideNext();
+        }
+    };
+
+    const handleSlideChange = (swiper) => {
+        setActiveIndex(swiper.realIndex);
+    };
 
     return(
         <div className={styles.root}>
+            <div className={styles.containerSwiper}>
+                <Swiper
+                    modules={[Navigation, Controller]}
+                    spaceBetween={-3}
+                    slidesPerView={3}
+                    centeredSlides={true}
+                    loop={true}
+                    onSwiper={(swiper) => {
+                        swiperRef.current = swiper;
+                    }}
+                    onSlideChange={handleSlideChange}
+                    className={styles.swiper}
+                >
+                    {characters.map((character, index) => (
+                        <SwiperSlide
+                            key={index}
+                            className={index === activeIndex ? styles.activeSlide : styles.inactiveSlide}
+                        >
+                            <div className={index === activeIndex ? styles.activeSlideImageWrapper : styles.inactiveSlideImageWrapper}>
+                                <Image
+                                    width={index === activeIndex ? 100 : 80}
+                                    height={index === activeIndex ? 194 : 155}
+                                    src={character.icon}
+                                    alt={character.name}
+                                    className={styles.icon}
+                                />
+                            </div>
+                        </SwiperSlide>
+                    ))}
+                </Swiper>
+                <div className={styles.navigation}>
+                    <button className={styles.navLeft} onClick={handleSlidePrev}>
+                        <Image src={'/Arrow.png'} alt={''} width={15} height={15} />
+                    </button>
+                    <div className={styles.caption}>
+                        <span>{characters[activeIndex].name}</span>
+                    </div>
+                    <button className={styles.navRight} onClick={handleSlideNext}>
+                        <Image src={'/Arrow.png'} alt={''} width={15} height={15} />
+                    </button>
+                </div>
+            </div>
             <Image src={bg} alt={''} className={styles.bg} width={450} height={1000} />
             <div className={styles.container}>
-                {friends.map((item, index) => <ListItem key={index} item={item} index={index+1} />)}
+                {ratingData[activeIndex].map((item, index) => <ListItem key={index} item={item} index={index+1} />)}
             </div>
         </div>
     )
 }
+
+
+
