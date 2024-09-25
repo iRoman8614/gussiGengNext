8b51fdd7c6d3d0505371c794648e0943c8c038b3
Roman Baranovskii <rom8614@gmail.com>
2024-09-25 12:55:27 +0300

    getRandom

--- src/pages/getRandom/index.js
+++ src/pages/getRandom/index.js
@@ -1,5 +1,6 @@
-import Image from "next/image";
 import React, {useEffect, useState} from "react";
+import Image from "next/image";
+import {useRouter} from "next/router";
 
 import styles from '@/styles/Random.module.scss'
 
@@ -14,26 +15,38 @@ const yellowCard = '/random/yellowCard.png'
 const redCard = '/random/redCard.png'
 
 export default function Page() {
+    const router = useRouter();
     const [showCard, setShowCard] = useState(false)
     const [teamId, setTeamId] = useState(1)
 
     useEffect(() => {
         if (typeof window !== "undefined") {
-            // Получаем init из localStorage
             const init = JSON.parse(localStorage.getItem("init"));
             if (init && init.group) {
-                setTeamId(init.group.id); // Устанавливаем команду
+                setTeamId(init.group.id);
             }
         }
         }, [])
 
+    useEffect(() => {
+        if (typeof window !== 'undefined' && window.Telegram?.WebApp?.BackButton) {
+            window.Telegram.WebApp.BackButton.show();
+            window.Telegram.WebApp.BackButton.onClick(() => {
+                router.push('/');
+            });
+            return () => {
+                window.Telegram.WebApp.BackButton.hide();
+            };
+        }
+    }, [router]);
+
     const ShownCard = ({state, teamId}) => {
         return(
             <div className={state ? styles.shown : styles.hidden}>
-                {teamId === 1 && <Image className={styles.cardImage1} src={greenCard} alt={''} width={200} height={340} />}
-                {teamId === 2 && <Image className={styles.cardImage2} src={blueCard} alt={''} width={200} height={340} />}
-                {teamId === 3 && <Image className={styles.cardImage3} src={yellowCard} alt={''} width={200} height={340} />}
-                {teamId === 4 && <Image className={styles.cardImage4} src={redCard} alt={''} width={200} height={340} />}
+                {teamId === 1 && <Image className={styles.cardImage1} src={greenCard} alt={''} width={200} height={340} loading="lazy" />}
+                {teamId === 2 && <Image className={styles.cardImage2} src={blueCard} alt={''} width={200} height={340} loading="lazy" />}
+                {teamId === 3 && <Image className={styles.cardImage3} src={yellowCard} alt={''} width={200} height={340} loading="lazy" />}
+                {teamId === 4 && <Image className={styles.cardImage4} src={redCard} alt={''} width={200} height={340} loading="lazy" />}
             </div>
         )
     }
