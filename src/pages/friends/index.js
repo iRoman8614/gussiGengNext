bf2494883b15709f1bd9cfdf0168476c533742d3
Roman Baranovskii <rom8614@gmail.com>
2024-10-03 17:52:32 +0300

    friends

--- src/pages/friends/index.js
+++ src/pages/friends/index.js
@@ -8,10 +8,13 @@ import styles from '@/styles/Friends.module.scss'
 
 const bg = '/backgrounds/friendsBG.png'
 const copy = '/copy.png'
+const star = '/Star.png'
+const money = '/money.png'
 
 export default function Page() {
     const [userId, setUserId] = useState(null);
     const [userName, setUserName] = useState('');
+    const [activeTab, setActiveTab] = useState(1);
 
     const friends = [
         {
@@ -130,31 +133,95 @@ export default function Page() {
             <Image className={styles.bg} src={bg} alt={'bg'} width={450} height={1000} />
             <div className={styles.container}>
                 <div className={styles.hintBlock}>
-                    <div>- Each referral gives you +1 PvP pass.</div>
-                    <div>- PvP Pass – allows 5 extra PvP games, bypassing the 6-hour cooldown.</div>
-                    <div>- If the referral has Telegram Premium, you get +2 PvP passes.</div>
-                    <div className={styles.important}>- you’ll earn 10% of the rewards your referrals rack up for completing in-game <a className={styles.point}>tasks</a>!</div>
-                </div>
-                <div className={styles.title}>friends</div>
-                <div className={styles.block}>
-                    <>
-                        {filteredFriends.length >= 3
-                            ?
-                            <div className={styles.barComplited} onClick={() => {router.push('/upgrades')}}><a>Claim</a></div>
-                            :
-                            <div className={styles.bar}>
-                                {filteredFriends.map((index) => <div key={index} className={styles.barItem}></div>)}
+                    <div className={styles.bannerSet}>
+                        <div className={styles.banner}>
+                            <div className={styles.bannerMid}>
+                                invite a friend
+                                <p>+1 PVP pass</p>
                             </div>
-                        }
-                        <div className={styles.sign}>{filteredFriends.length}/3</div>
-                    </>
-                    <div className={styles.list}>{filteredFriends.map((item, index) => <ListItem key={index} item={item} />)}</div>
-                    <div className={styles.buttonset}>
-                        <button className={styles.btnInvite} onClick={inviteClick}>INVITE</button>
-                        <button className={styles.btnCopy} onClick={handleClick}>
-                            <Image src={copy} alt={'copy'} height={50} width={50} />
-                        </button>
+                            <div className={styles.hintLabel}><a>+5 extra games</a></div>
+                        </div>
+                        <div className={styles.banner}>
+                            <div className={styles.bannerMid}>
+                                invite a
+                                tg <Image src={star} alt={''} width={20} height={20} />
+                                friend
+                                <p>+2 PVP pass</p>
+                            </div>
+                            <div className={styles.hintLabel}><a>+10 extra games</a></div>
+                        </div>
                     </div>
+                    <div className={styles.hintLabel}>earn <a>10%</a> of your friends <a>tasks</a> completion!</div>
+                </div>
+                <div className={styles.block}>
+                    <div className={styles.buttonSet}>
+                        <div className={styles.folderBtnStats}
+                             style={{
+                                 zIndex: activeTab === 1 ? 112 : 110,
+                                 marginBottom:  activeTab === 1 ? '0px' : '-12px',
+                                 borderRight:  activeTab === 1 ? '2px solid #3842a4' : 'none',
+                             }}
+                             onClick={() => setActiveTab(1)}>friends</div>
+                        <div
+                            className={styles.folderBtnSkins}
+                            style={{
+                                zIndex: activeTab === 2 ? 113 : 110,
+                                marginBottom:  activeTab === 2 ? '-0px' : '2px',
+                            }}
+                            onClick={() => setActiveTab(2)}
+                        >info
+                        </div>
+                    </div>
+                    {activeTab === 1 &&<div className={styles.friendsContainer}>
+                        <div className={styles.list}>
+                            {filteredFriends.map((item, index) => <ListItem key={index} item={item} />)}
+                        </div>
+                        <div className={styles.buttonset}>
+                            <button className={styles.btnInvite} onClick={inviteClick}>INVITE</button>
+                            <button className={styles.btnCopy} onClick={handleClick}>
+                                <Image src={copy} alt={'copy'} height={50} width={50} />
+                            </button>
+                        </div>
+                    </div>}
+                    {activeTab === 2 && <div className={styles.infoContainer}>
+                        <div className={styles.listInfo}>
+                            <div className={styles.hintLabel}>- Each <a>referral</a> gives you <a>+1 PvP pass.</a></div>
+                            <div className={styles.hintLabel}>- <a>PvP Pass</a> – allows <a>5 extra PvP games</a>, bypassing the 6-hour cooldown.</div>
+                            <div className={styles.hintLabel}>- If the referral has <a>Telegram Premium</a>, you get <a>+2 PvP passes</a>.</div>
+                            <div className={styles.hintLabel}>Referrals <a>score 10,000 </a> <Image src={money} alt={''} width={15} height={15} />    just for <a>signing up</a> (joining the bot) through your referral link!</div>
+                            <div className={styles.title}>rewards</div>
+                            <div className={styles.rewards}>
+                                <div className={styles.row}>
+                                    <div className={styles.hintLabel}><a>1</a> referral</div>
+                                    <div className={styles.hintLabel}><a>10000 </a><Image src={money} alt={''} width={15} height={15} /></div>
+                                </div>
+                                <div className={styles.row}>
+                                    <div className={styles.hintLabel}><a>3</a> referrals</div>
+                                    <div className={styles.hintLabel}><a>50000 </a><Image src={money} alt={''} width={15} height={15} /></div>
+                                </div>
+                                <div className={styles.row}>
+                                    <div className={styles.hintLabel}><a>5</a> referrals</div>
+                                    <div className={styles.hintLabel}><a>100000 </a><Image src={money} alt={''} width={15} height={15} /></div>
+                                </div>
+                                <div className={styles.row}>
+                                    <div className={styles.hintLabel}><a>10</a> referrals</div>
+                                    <div className={styles.hintLabel}><a>250000 </a><Image src={money} alt={''} width={15} height={15} /></div>
+                                </div>
+                                <div className={styles.row}>
+                                    <div className={styles.hintLabel}><a>25</a> referrals</div>
+                                    <div className={styles.hintLabel}><a>750000 </a><Image src={money} alt={''} width={15} height={15} /></div>
+                                </div>
+                                <div className={styles.row}>
+                                    <div className={styles.hintLabel}><a>50</a> referrals</div>
+                                    <div className={styles.hintLabel}><a>1500000 </a><Image src={money} alt={''} width={15} height={15} /></div>
+                                </div>
+                                <div className={styles.row}>
+                                    <div className={styles.hintLabel}><a>100</a> referrals</div>
+                                    <div className={styles.hintLabel}><a>5000000 </a><Image src={money} alt={''} width={15} height={15} /></div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>}
                 </div>
             </div>
         </div>
