3306ce2a880c97f040184a94e851278d6d50f8af
Roman Baranovskii <rom8614@gmail.com>
2024-10-08 10:59:44 +0300

    upgrades modal

--- src/components/itemPlaceholder/ItemPlaceholder.jsx
+++ src/components/itemPlaceholder/ItemPlaceholder.jsx
@@ -3,19 +3,21 @@ import styles from './ItemPlaceholder.module.scss';
 
 export const ItemPlaceholder = ({ item, onClick }) => {
     return (
-        <div className={styles.root} onClick={() => onClick(item)}>
-            {item ? (
-                <>
-                    <div className={styles.title}>{item.Name}</div>
-                    <div className={styles.details}>Cost: {item.Cost}</div>
-                    <div className={styles.details}>Card level: {item.Level}</div>
-                </>
-            ) : (
-                <>
-                    <div className={styles.icon}></div>
-                    <div className={styles.title}>Item name</div>
-                </>
-            )}
-        </div>
+        <>
+            <div className={styles.root} onClick={onClick}>
+                {item ? (
+                    <>
+                        <div className={styles.title}>{item.Name}</div>
+                        <div className={styles.details}>Cost: {item.Cost}</div>
+                        <div className={styles.details}>Card level: {item.Level}</div>
+                    </>
+                ) : (
+                    <>
+                        <div className={styles.icon}></div>
+                        <div className={styles.title}>Item name</div>
+                    </>
+                )}
+            </div>
+        </>
     );
 };
