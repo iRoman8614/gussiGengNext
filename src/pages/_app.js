d704e54e03cb1c11788931c0fe302b34e4e31bb8
Roman Baranovskii <rom8614@gmail.com>
2024-10-09 17:54:20 +0300

    scale

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -2,10 +2,14 @@ import "@/styles/globals.scss";
 import Script from "next/script";
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import Head from "next/head";
 
 export default function App({ Component, pageProps }) {
   return(
       <>
+          <Head>
+              <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
+          </Head>
         <Script
             src="https://telegram.org/js/telegram-web-app.js"
             strategy="beforeInteractive"
