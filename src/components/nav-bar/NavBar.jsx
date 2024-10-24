4b9cddce64719dbe351c33ce87ad872f2ff321fd
Roman Baranovskii <rom8614@gmail.com>
2024-10-24 22:03:52 +0300

    icons

--- src/components/nav-bar/NavBar.jsx
+++ src/components/nav-bar/NavBar.jsx
@@ -2,14 +2,13 @@ import { useState, useEffect } from 'react';
 import { useRouter } from 'next/router';
 import { IconButton } from "../buttons/icon-btn/IconButton.jsx";
 import { BigButton } from "../buttons/big-btn/BigButton.jsx";
-import { toast } from "react-toastify";
-import axiosInstance from '@/utils/axios';
 
 const home = '/main-buttons/home.png';
 const upgrades = '/main-buttons/upgrades.png';
 const hands = '/main-buttons/hands.png';
 const friends = '/main-buttons/friends.png';
 const bag = '/main-buttons/bag.png';
+const FAQ = '/main-buttons/FAQ.png'
 
 import styles from './NavBar.module.scss';
 import 'react-toastify/dist/ReactToastify.css';
@@ -19,11 +18,11 @@ export const NavBar = () => {
 
     return (
         <div className={styles.root}>
-            <IconButton image={home} alt={'home'} title={'home'} onClick={() => {router.push('/main')}} />
+            <IconButton image={bag} alt={'items'} title={'items'} hidden={true} onClick={() => {router.push('/main')}} />
             <IconButton image={upgrades} alt={'upgrades'} title={'exp'} onClick={() => {router.push('/upgrades')}} />
             <BigButton image={hands} alt={'pvp'} title={'pvp'} onClick={() => {router.push('/lobby')}} />
             <IconButton image={friends} alt={'friends'} title={'friends'} onClick={() => {router.push('/friends')}} />
-            <IconButton image={bag} alt={'items'} title={'items'} hidden={true} onClick={() => {router.push('/main')}} />
+            <IconButton image={FAQ} alt={'home'} title={'FAQ'} hidden={true} onClick={() => {router.push('/main')}} />
         </div>
     );
 };
