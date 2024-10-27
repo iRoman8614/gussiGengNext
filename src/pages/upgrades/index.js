75ae3920c6e160aa48103442874b86e8eaef1130
Roman Baranovskii <rom8614@gmail.com>
2024-10-27 12:22:58 +0300

    tasks

--- src/pages/upgrades/index.js
+++ src/pages/upgrades/index.js
@@ -78,6 +78,54 @@ export default function Page() {
         return originalName;
     };
 
+    // useEffect(() => {
+    //     const fetchTasksAndFriends = async () => {
+    //         try {
+    //             const statsResponse = await axiosInstance.get('/profile/stats');
+    //             const stats = statsResponse.data;
+    //             const friendsResponse = await axiosInstance.get('/profile/my-invitees');
+    //             const numFriends = friendsResponse.data.length;
+    //             const tasksResponse = await axiosInstance.get('/task/all');
+    //             let tasks = tasksResponse.data;
+    //             const completedTasksResponse = await axiosInstance.get('/task/completed-tasks');
+    //             const completedTasks = completedTasksResponse.data.map(task => task.task.id);
+    //             tasks = tasks.map(task => {
+    //                 const isCompleted = completedTasks.includes(task.id);
+    //                 if (task.type === 1) {
+    //                     return {
+    //                         ...task,
+    //                         current: numFriends,
+    //                         completed: isCompleted || numFriends >= task.amount,
+    //                         path: '/friends'
+    //                     };
+    //                 } else if (task.type === 2) {
+    //                     return {
+    //                         ...task,
+    //                         name: mapTaskName(task.name),
+    //                         url: task.name.includes("TG") ? "https://t.me/gang_wars_game" : "https://x.com/gangwars_game",
+    //                         completed: isCompleted
+    //                     };
+    //                 } else if (task.type === 5) {
+    //                     return {
+    //                         ...task,
+    //                         current: stats.victory,
+    //                         completed: isCompleted || stats.victory >= task.amount,
+    //                         path: '/pvp'
+    //                     };
+    //                 }
+    //                 return {
+    //                     ...task,
+    //                     completed: isCompleted
+    //                 };
+    //             });
+    //             setTasks(tasks);
+    //         } catch (error) {
+    //             console.error('Ошибка при загрузке данных:', error);
+    //         }
+    //     };
+    //     fetchTasksAndFriends();
+    // }, []);
+
     useEffect(() => {
         const fetchTasksAndFriends = async () => {
             try {
@@ -89,43 +137,49 @@ export default function Page() {
                 let tasks = tasksResponse.data;
                 const completedTasksResponse = await axiosInstance.get('/task/completed-tasks');
                 const completedTasks = completedTasksResponse.data.map(task => task.task.id);
+                const lastCompletedTaskId = Math.max(...tasks.filter(t => t.type === 1 && completedTasks.includes(t.id)).map(t => t.id));
+
                 tasks = tasks.map(task => {
                     const isCompleted = completedTasks.includes(task.id);
                     if (task.type === 1) {
+                        const isVisible = task.id <= lastCompletedTaskId + 1;
                         return {
                             ...task,
                             current: numFriends,
                             completed: isCompleted || numFriends >= task.amount,
-                            path: '/friends'
+                            path: '/friends',
+                            visible: isVisible
                         };
                     } else if (task.type === 2) {
                         return {
                             ...task,
                             name: mapTaskName(task.name),
                             url: task.name.includes("TG") ? "https://t.me/gang_wars_game" : "https://x.com/gangwars_game",
-                            completed: isCompleted
+                            completed: isCompleted,
+                            visible: true
                         };
                     } else if (task.type === 5) {
                         return {
                             ...task,
                             current: stats.victory,
                             completed: isCompleted || stats.victory >= task.amount,
-                            path: '/pvp'
+                            path: '/pvp',
+                            visible: true
                         };
                     }
                     return {
                         ...task,
-                        completed: isCompleted
+                        completed: isCompleted,
+                        visible: true
                     };
                 });
-                setTasks(tasks);
+                setTasks(tasks.filter(task => task.visible));
             } catch (error) {
                 console.error('Ошибка при загрузке данных:', error);
             }
         };
         fetchTasksAndFriends();
     }, []);
-
 
 
     useEffect(() => {
