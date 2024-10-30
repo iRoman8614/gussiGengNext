de3345842059365209acf40e0f93ee4db4ac16df
Roman Baranovskii <rom8614@gmail.com>
2024-10-30 19:56:55 +0300

    visual fixes2

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -28,7 +28,7 @@ export default function App({ Component, pageProps }) {
                 draggable
                 pauseOnHover
             />
-            {/*<MobileGuard />*/}
+            <MobileGuard />
             <Component {...pageProps} />
             <BackgroundPreloader />
         </AssetsCacheProvider>
