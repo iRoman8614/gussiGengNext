1e44813ba8fa8540b3fd07a958d5160596f147c6
Roman Baranovskii <rom8614@gmail.com>
2024-11-01 01:09:06 +0300

    friends text fix3

--- src/pages/friends/index.js
+++ src/pages/friends/index.js
@@ -140,7 +140,7 @@ export default function Page() {
                     </div>
                 </div>}
                 {activeTab === 1 && <div className={styles.hintBg}>
-                    <div className={styles.hintLabel}>earn <a>10%</a> of your friends <a>tasks</a> completion!</div>
+                    <div className={styles.hintLabel2}>earn <a>10%</a> of your friends <a>tasks</a> completion!</div>
                 </div>}
                 <div className={activeTab === 1 ? styles.block : styles.block2}>
                     <div className={styles.buttonSet}>
@@ -197,7 +197,7 @@ export default function Page() {
                             <div className={styles.hintLabel}>-<a>PvP Pass</a> – allows <a>5 extra PvP games</a>, bypassing the 6-hour cooldown.</div>
                             <div className={styles.hintLabel}>-If the referral has <a>Telegram Premium</a>, you get <a>+2 PvP passes</a>.</div>
                             <div className={styles.hintLabel}>Referrals <a>score 10,000 </a> <Image src={money} alt={''} width={15} height={15} />    just for <a>signing up</a> (joining the bot) through your referral link!</div>
-                            <div className={styles.title}>rewards</div>
+                            <div className={styles.title}>Rewards</div>
                             <div className={styles.rewards}>
                                 <div className={styles.row}>
                                     <div className={styles.hintLabel}><a>1</a> referral</div>
