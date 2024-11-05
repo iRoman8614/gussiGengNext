cdbb43a7d3947f53e0d52bdd7febfdc5c10036c3
Roman Baranovskii <rom8614@gmail.com>
2024-11-05 15:24:25 +0300

    boards ref

--- src/pages/boards/index.js
+++ src/pages/boards/index.js
@@ -61,9 +61,6 @@ export default function Page() {
         }
     }, [activeIndex, leaderData, fetchLeaderboard]);
 
-    useEffect(() => {
-        fetchStats()
-    }, []);
 
 
     useEffect(() => {
