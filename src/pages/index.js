473893661aa6342ac9318b81d1bf9f88736eee21
Roman Baranovskii <rom8614@gmail.com>
2024-11-08 15:25:44 +0300

    fixes2

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,73 +3,53 @@ import { useRouter } from 'next/router';
 import Image from 'next/image';
 import Head from "next/head";
 import { toast } from "react-toastify";
-import { useAssetsCache } from '@/context/AssetsCacheContext';
 import { useInit } from '@/context/InitContext';
-import { useProfileInit, useProfileStats, useFarmStart } from '@/utils/api';
+import { useProfileInit, useFarmStart } from '@/utils/api';
 
 import styles from '@/styles/Loader.module.scss';
 
 const loaderImage = '/loadingImg.jpg';
 
-const backgroundAssets = [
-    '/backgrounds/backalley.png',
-    '/backgrounds/leaderboardBG.png',
-    '/backgrounds/Lobby.png',
-    '/backgrounds/nightcity.png',
-    '/backgrounds/randomBG.png'
-];
-
 const newPlayerAssets = [
-    ...backgroundAssets,
-    '/random/blueCard.png', '/random/card.png',
-    '/random/dialog.png', '/random/dialog2.png',
-    '/random/greenCard.png', '/random/hand.png',
-    '/random/oneCard.png', '/random/person.png',
-    '/random/redCard.png', '/random/yellowCard.png',
-    '/upgradesCards/limit/limit1.png', '/upgradesCards/limit/limit2.png',
-    '/upgradesCards/limit/limit3.png', '/upgradesCards/rate/rate1.png',
-    '/upgradesCards/rate/rate2.png', '/upgradesCards/rate/rate3.png',
-    '/upgradesCards/slider/limitSlide.png', '/upgradesCards/slider/rateSlide.png',
-];
-
-const experiencedPlayerAssets = [
-    ...backgroundAssets,
-    '/main-buttons/account.png', '/main-buttons/bag.png',
-    '/main-buttons/boards.png', '/main-buttons/FAQ.png',
-    '/main-buttons/friends.png', '/main-buttons/hand2.png',
-    '/main-buttons/hands.png', '/main-buttons/home.png',
-    '/main-buttons/rich.png', '/main-buttons/settings.png',
-    '/main-buttons/upgrades.png', '/main-buttons/wallet.png',
-    '/upgradesCards/limit/limit1.png', '/upgradesCards/limit/limit2.png',
-    '/upgradesCards/limit/limit3.png', '/upgradesCards/rate/rate1.png',
-    '/upgradesCards/rate/rate2.png', '/upgradesCards/rate/rate3.png',
-    '/upgradesCards/slider/limitSlide.png', '/upgradesCards/slider/rateSlide.png'
+    '/backgrounds/backalley.png', '/backgrounds/leaderboardBG.png',
+    '/backgrounds/Lobby.png', '/backgrounds/nightcity.png',
+    '/backgrounds/randomBG.png', '/random/blueCard.png',
+    '/random/card.png', '/random/dialog.png', '/random/dialog2.png',
+    '/random/greenCard.png', '/random/hand.png', '/random/oneCard.png',
+    '/random/person.png', '/random/redCard.png', '/random/yellowCard.png',
 ];
 
 export default function LoaderPage() {
     const router = useRouter();
-    const { preloadAssets } = useAssetsCache();
     const { updateContext } = useInit();
     const [isNewPlayer, setIsNewPlayer] = useState(false);
     const [dataFetched, setDataFetched] = useState(false);
     const [authToken, setAuthToken] = useState(null);
-    const [hasError, setHasError] = useState(false);
 
     const CURRENT_VERSION = process.env.NEXT_PUBLIC_CURRENT_VERSION
 
     const { fetchProfileInit } = useProfileInit(authToken);
-    const { fetchProfileStats } = useProfileStats();
     const { fetchFarmStart } = useFarmStart();
+
+    useEffect(() => {
+        const { token } = router.query;
+        if (token) {
+            localStorage.setItem('authToken', token);
+            setAuthToken(token);
+        } else {
+            console.error("Unauthorized");
+        }
+    }, [router.query]);
+
+    const updateBodyHeight = useCallback(() => {
+        document.body.style.height = `${window.innerHeight}px`;
+    }, []);
 
     const initializeTelegramWebApp = useCallback(() => {
         if (window.Telegram?.WebApp) {
             window.Telegram.WebApp.setHeaderColor('#183256');
             window.Telegram.WebApp.expand();
-            document.body.style.height = `${window.innerHeight}px`;
-            window.addEventListener('resize', () => {
-                document.body.style.height = `${window.innerHeight}px`;
-            });
-            return true;
+            updateBodyHeight();
             // const search = window.Telegram.WebApp.initData;
             // const urlParams = new URLSearchParams(search);
             // const userParam = urlParams.get('user');
@@ -77,14 +57,16 @@ export default function LoaderPage() {
             //     const decodedUserParam = decodeURIComponent(userParam);
             //     const userObject = JSON.parse(decodedUserParam);
             // }
+            window.addEventListener('resize', updateBodyHeight);
         } else {
             toast.error("Telegram WebApp unavailable");
-            return false;
         }
-    }, []);
+        checkVersion();
+    }, [updateBodyHeight]);
 
     const checkVersion = useCallback(() => {
         if (typeof window !== 'undefined') {
+            console.log('проверка версии бд')
             const savedVersion = localStorage.getItem('version');
             if (savedVersion !== CURRENT_VERSION) {
                 localStorage.clear();
@@ -96,81 +78,92 @@ export default function LoaderPage() {
 
     const checkLocalStorage = useCallback(() => {
         if (typeof window !== 'undefined') {
+            console.log("проверка ls")
             const init = localStorage.getItem('init');
             const start = localStorage.getItem('farm');
             const GWToken = localStorage.getItem('GWToken');
-            const playerStatus = init && start && GWToken;
-            console.log('playerStatus', playerStatus)
-            setIsNewPlayer(!playerStatus);
-            return playerStatus;
+
+            if (!init || !start || !GWToken) {
+                setIsNewPlayer(true);
+                return false;
+            }
+            return true;
         }
+        return false;
     }, []);
 
     const fetchData = useCallback(async () => {
-        try {
-            const {data: initData, error: initError} = await fetchProfileInit();
-            console.log('initData', initData)
-            if (initError) {
-                throw new Error('Error during profile initialization');
-                setHasError(true);
-            }
-            await fetchFarmStart();
-            setHasError(false);
-        } catch (error) {
-            setHasError(true);
-            if (error.response?.status === 401 || error?.status === 401 ) {
-                toast.error('Unauthorized: Please check your token and try again.');
-                return
-            } else {
-                toast.error('Error during data fetching. Please try again.');
-                return
+        if (!dataFetched && authToken) {
+            console.log('запросы')
+            try {
+                const {data, error: initError } = await fetchProfileInit()
+                if (initError) {
+                    throw new Error('Initialization failed, restart app');
+                }
+                await fetchFarmStart()
+                setDataFetched(true);
+            } catch (error) {
+                if(error.status === 401) {
+                    toast.error('error during init request, restart app');
+                    return;
+                }
+                return;
             }
         }
-    }, [dataFetched, authToken, fetchProfileInit, fetchFarmStart, fetchProfileStats]);
+    }, [dataFetched, authToken]);
 
-    const loadAssets = useCallback(async () => {
-        await preloadAssets(isNewPlayer ? newPlayerAssets : experiencedPlayerAssets);
-    }, [isNewPlayer, preloadAssets]);
 
     const updateAndRedirect = useCallback(() => {
+        console.log('upadete ls')
         const savedInit = JSON.parse(localStorage.getItem('init'));
         const savedFarm = JSON.parse(localStorage.getItem('farm'));
+        const isExperiencedPlayer = savedInit && savedFarm
         updateContext();
-        if (savedInit?.groupId === 0 || savedFarm?.farmLimit === 0) return;
-        router.push(isNewPlayer ? '/getRandom' : '/main');
-    }, [isNewPlayer, router, updateContext]);
+        if (savedInit.groupId === 0 || savedFarm.farmLimit === 0) {
+            return;
+        }
+        if (isNewPlayer) {
+            console.log('to getRandom')
+            router.push('/getRandom');
+        } else if (isExperiencedPlayer) {
+            console.log('to main')
+            router.push('/main');
+        }
+    }, [isNewPlayer]);
 
     useEffect(() => {
         const { token } = router.query;
-        if (token) {
+        const executeAfterToken = async (token) => {
+            initializeTelegramWebApp()
             localStorage.setItem('authToken', token);
-            setAuthToken(token);
+            await new Promise((resolve) => {
+                localStorage.setItem('authToken', token);
+                resolve();
+            });
+            const hasData = checkLocalStorage();
+            if (!hasData) {
+                await fetchData();
+            }
+            updateAndRedirect();
+        };
+        if (token) {
+            executeAfterToken(token);
+        } else {
+            return
         }
     }, [router.query, authToken]);
 
-    useEffect(() => {
-        if (authToken) {
-            if (initializeTelegramWebApp()) {
-                checkVersion();
-                checkLocalStorage();
-                const loadDataAndAssets = async () => {
-                    await fetchData();
-                    if (!hasError) {
-                        updateAndRedirect();
-                    }
-                };
-                loadDataAndAssets();
-            }
-        }
-    }, [router.query, authToken]);
 
     return (
         <>
             <Head>
                 <link rel="preload" href={loaderImage} as="image" />
+                {newPlayerAssets.map((src, index) => (
+                    <link key={index} rel="preload" href={src} as="image" />
+                ))}
             </Head>
             <div className={styles.root}>
-                <Image className={styles.video} src={loaderImage} alt="Loading..." width={500} height={500} />
+                <Image className={styles.video} src={loaderImage} alt="Loading..." width={500} height={500} priority />
                 <LoadingText />
             </div>
         </>
