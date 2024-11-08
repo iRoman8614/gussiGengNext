951057d24d3c9904e97317d45e231c79c7717fd0
Roman Baranovskii <rom8614@gmail.com>
2024-11-08 15:20:40 +0300

    fixes2

--- src/pages/index.js
+++ src/pages/index.js
@@ -121,15 +121,9 @@ export default function LoaderPage() {
             setHasError(true);
             if (error.response?.status === 401 || error?.status === 401 ) {
                 toast.error('Unauthorized: Please check your token and try again.');
-                if (window.Telegram?.WebApp) {
-                    window.Telegram.WebApp.close();
-                }
                 return
             } else {
                 toast.error('Error during data fetching. Please try again.');
-                if (window.Telegram?.WebApp) {
-                    window.Telegram.WebApp.close();
-                }
                 return
             }
         }
