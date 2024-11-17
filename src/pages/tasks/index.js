3fe6115f9ae066f16b93d040bab1d1c3f76f1b34
Roman Baranovskii <rom8614@gmail.com>
2024-11-17 21:30:30 +0300

    tasks+upgrades

---
+++ src/pages/tasks/index.js
@@ -0,0 +1,211 @@
+import {useEffect, useRef, useState} from 'react';
+import Image from "next/image";
+import {useRouter} from "next/router";
+import {TaskBtn} from "@/components/taskBtn/TaskBtn";
+import axiosInstance from '@/utils/axios';
+import {useTranslation} from "react-i18next";
+
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/controller';
+import styles from '@/styles/Upgrades.module.scss'
+import {Controller, Navigation} from "swiper/modules";
+import {toast} from "react-toastify";
+import {useInit} from "@/context/InitContext";
+import {useFarmCollect} from "@/utils/api";
+
+const money = '/money.png'
+
+export default function Page() {
+    const router = useRouter();
+    const { t } = useTranslation();
+    const { coins } = useInit();
+    const { tab } = router.query;
+    const [balance, setBalance] = useState(0);
+    const [tasks, setTasks] = useState([]);
+
+
+    useEffect(() => {
+        const fetchTasksAndFriends = async () => {
+            try {
+                const statsResponse = await axiosInstance.get('/profile/stats');
+                const stats = statsResponse.data;
+                const friendsResponse = await axiosInstance.get('/profile/my-invitees');
+                const numFriends = friendsResponse.data.length;
+                const tasksResponse = await axiosInstance.get('/task/all');
+                let tasks = tasksResponse.data;
+                const completedTasksResponse = await axiosInstance.get('/task/completed-tasks');
+                const completedTasks = completedTasksResponse.data.map(task => task.task.id);
+                const lastCompletedTaskIdType1 = Math.max(0, ...tasks.filter(task => task.type === 1 && completedTasks.includes(task.id)).map(task => task.id));
+                const type3Tasks = tasks
+                    .filter(task => task.type === 3)
+                    .sort((a, b) => a.id - b.id);
+                const lastCompletedTaskIndexType3 = type3Tasks.findIndex(task => task.id === Math.max(...completedTasks.filter(id => type3Tasks.some(task => task.id === id))));
+                tasks = tasks.map(task => {
+                    const isCompleted = completedTasks.includes(task.id);
+                    let readyToComplete = false;
+                    let icon = '';
+                    if (task.type === 1 && numFriends >= task.amount && !isCompleted) {
+                        readyToComplete = true;
+                    }
+                    if (task.type === 3 && stats.victory >= task.amount && !isCompleted) {
+                        readyToComplete = true;
+                    }
+                    if (task.type === 2) {
+                        icon = task.name.includes("TG") ? "tg" : task.name.includes("X") ? "x" : '';
+                    }
+                    const isVisible = (task.type === 1 && task.id <= lastCompletedTaskIdType1 + 1) ||
+                        (task.type === 3 && (isCompleted || type3Tasks[lastCompletedTaskIndexType3 + 1]?.id === task.id)) ||
+                        (task.type !== 1 && task.type !== 3);
+
+                    return {
+                        ...task,
+                        name: mapTaskName(task.name),
+                        current: task.type === 1 ? numFriends : stats.victory,
+                        completed: isCompleted,
+                        path: task.type === 1 ? '/friends' : '/lobby',
+                        visible: isVisible,
+                        readyToComplete: readyToComplete,
+                        icon: icon,
+                    };
+                });
+
+                setTasks(tasks.filter(task => task.visible));
+            } catch (error) {
+                console.error('Ошибка при загрузке данных:', error);
+            }
+        };
+        fetchTasksAndFriends();
+    }, []);
+
+    const mapTaskName = (originalName) => {
+        if (originalName.includes("TG")) {
+            return 'sub to GW telegram';
+        } else if (originalName.includes("twitter")) {
+            return 'sub to Gw x';
+        }
+        return originalName;
+    };
+
+
+    const timerRef = useRef(null);
+    const handleTaskClick = (task) => {
+        if (task.readyToComplete) {
+            executeTask(task.id);
+            task.readyToComplete = false
+        } else {
+            switch (task.type) {
+                case 1:
+                    navigateToPage(task.path);
+                    break;
+                case 2:
+                    let url = '';
+                    if (task.id === 8) {
+                        url = "https://t.me/gang_wars_game";
+                    } else if (task.id === 9 || task.name && (task.name.toLowerCase().includes("x") || task.name.toLowerCase().includes("twitter"))) {
+                        url = "https://x.com/gangwars_game";
+                    } else {
+                        console.error('URL could not be determined. Task name:', task.name);
+                    }
+                    if (url) {
+                        window.open(url, '_blank');
+                        timerRef.current = setTimeout(() => executeTask(task.id), 500);
+                    }
+                    break;
+                case 3:
+                    navigateToPage(task.path);
+                    break;
+                default:
+                    console.log('No action for this task.');
+            }
+        }
+    };
+    useEffect(() => {
+        return () => {
+            if (timerRef.current) {
+                clearTimeout(timerRef.current);
+            }
+        };
+    }, []);
+    const executeTask = async (taskId) => {
+        try {
+            await axiosInstance.get(`/task/execute?taskId=${taskId}`);
+            fetchCompletedTasks();
+        } catch (error) {
+            console.error(`Error executing task ${taskId}:`, error);
+        }
+    }
+
+    const fetchCompletedTasks = async () => {
+        try {
+            const response = await axiosInstance.get('/task/completed-tasks');
+            const completedTaskIds = response.data.map(task => task.task.id);
+            setTasks(tasks.map(task => ({
+                ...task,
+                completed: completedTaskIds.includes(task.id)
+            })));
+        } catch (error) {
+            console.error('Error fetching completed tasks:', error);
+        }
+    };
+
+    useEffect(() => {
+        if (typeof window !== "undefined") {
+            const start = JSON.parse(localStorage.getItem("start"));
+            if (start) {
+                setBalance(start.coins);
+            }
+        }
+    }, []);
+
+    useEffect(() => {
+        if (typeof window !== 'undefined' && window.Telegram?.WebApp?.BackButton) {
+            window.Telegram.WebApp.BackButton.show();
+            window.Telegram.WebApp.BackButton.onClick(() => {
+                router.push('/main');
+            });
+            return () => {
+                window.Telegram.WebApp.BackButton.hide();
+            };
+        }
+    }, [router]);
+
+    function formatNumberFromEnd(num) {
+        return num.toString().replace(/(\d)(?=(\d{3})+$)/g, "$1 ");
+    }
+
+    return (
+        <div className={styles.root}>
+            <div className={styles.container}>
+                <div className={styles.balanceContainer}>
+                    <div className={styles.title}>tasks</div>
+                    <div className={styles.balance}>{formatNumberFromEnd(coins)}{' '}<Image src={money} alt={''} width={21} height={21} loading="lazy" /></div>
+                </div>
+                <div className={styles.block}>
+                    <div className={styles.skinContainer}>
+                        <div className={styles.col}>
+                            <div className={styles.label}>{t('EXP.main')}</div>
+                            {tasks.map((task, index) => {
+                                return(
+                                    <>
+                                        {task.type !== 4 && <TaskBtn
+                                            subtitle={task.name}
+                                            desc={task.type !== 2 ? `${task.current} / ${task.amount}` : ''}
+                                            completed={task.completed}
+                                            key={index}
+                                            icon={task.icon}
+                                            type={task.type}
+                                            readyToComplete={task.readyToComplete}
+                                            reward={formatNumberFromEnd(task.reward)}
+                                            onClick={() => handleTaskClick(task)}
+                                        />}
+                                    </>
+                                )
+                            })}
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
--- src/pages/upgrades/index.js
+++ src/pages/upgrades/index.js
@@ -1,8 +1,7 @@
-import React, {useEffect, useRef, useState} from 'react';
+import {useEffect, useRef, useState} from 'react';
 import Image from "next/image";
 import {useRouter} from "next/router";
 import {ItemPlaceholder} from "@/components/itemPlaceholder/ItemPlaceholder";
-import {TaskBtn} from "@/components/taskBtn/TaskBtn";
 import axiosInstance from '@/utils/axios';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import {useTranslation} from "react-i18next";
@@ -22,20 +21,15 @@ export default function Page() {
     const router = useRouter();
     const { t } = useTranslation();
     const { coins, updateContext, limit, rate } = useInit();
-    const { tab } = router.query;
-    const [activeTab, setActiveTab] = useState(tab || '1');
     const { collectAndStart } = useFarmCollect();
 
     const swiperRef = useRef(null);
     const [balance, setBalance] = useState(0);
-    const [isModalOpen, setIsModalOpen] = useState(false);
-
     const [limitLevels, setLimitLevels] = useState([]);
     const [rateLevels, setRateLevels] = useState([]);
     const [isUpgradeModalOpen, setIsUpgradeModalOpen] = useState(false);
     const [selectedItem, setSelectedItem] = useState(null);
     const [activeIndex, setActiveIndex] = useState(0);
-    const [tasks, setTasks] = useState([]);
 
     const sliderImages = [
         '/upgradesCards/slider/rateSlide.png',
@@ -77,71 +71,6 @@ export default function Page() {
         updateContext()
     }, [rate, limit])
 
-    useEffect(() => {
-        const fetchTasksAndFriends = async () => {
-            try {
-                const statsResponse = await axiosInstance.get('/profile/stats');
-                const stats = statsResponse.data;
-                const friendsResponse = await axiosInstance.get('/profile/my-invitees');
-                const numFriends = friendsResponse.data.length;
-                const tasksResponse = await axiosInstance.get('/task/all');
-                let tasks = tasksResponse.data;
-                const completedTasksResponse = await axiosInstance.get('/task/completed-tasks');
-                const completedTasks = completedTasksResponse.data.map(task => task.task.id);
-                const lastCompletedTaskIdType1 = Math.max(0, ...tasks.filter(task => task.type === 1 && completedTasks.includes(task.id)).map(task => task.id));
-                const lastCompletedTaskIdType3 = Math.max(0, ...tasks.filter(task => task.type === 3 && completedTasks.includes(task.id)).map(task => task.id));
-                const type3Tasks = tasks
-                    .filter(task => task.type === 3)
-                    .sort((a, b) => a.id - b.id);
-                const lastCompletedTaskIndexType3 = type3Tasks.findIndex(task => task.id === Math.max(...completedTasks.filter(id => type3Tasks.some(task => task.id === id))));
-                tasks = tasks.map(task => {
-                    const isCompleted = completedTasks.includes(task.id);
-                    let readyToComplete = false;
-                    let icon = '';
-                    if (task.type === 1 && numFriends >= task.amount && !isCompleted) {
-                        readyToComplete = true;
-                    }
-                    if (task.type === 3 && stats.victory >= task.amount && !isCompleted) {
-                        readyToComplete = true;
-                    }
-                    if (task.type === 2) {
-                        icon = task.name.includes("TG") ? "tg" : task.name.includes("X") ? "x" : '';
-                    }
-                    // const isVisible = (task.type === 1 ? task.id <= lastCompletedTaskIdType1 + 1 :
-                    //     (task.type === 3 ? task.id <= lastCompletedTaskIdType3 + 1 : true))
-                    const isVisible = (task.type === 1 && task.id <= lastCompletedTaskIdType1 + 1) ||
-                        (task.type === 3 && (isCompleted || type3Tasks[lastCompletedTaskIndexType3 + 1]?.id === task.id)) ||
-                        (task.type !== 1 && task.type !== 3);
-
-                    return {
-                        ...task,
-                        name: mapTaskName(task.name),
-                        current: task.type === 1 ? numFriends : stats.victory,
-                        completed: isCompleted,
-                        path: task.type === 1 ? '/friends' : '/lobby',
-                        visible: isVisible,
-                        readyToComplete: readyToComplete,
-                        icon: icon,
-                    };
-                });
-
-                setTasks(tasks.filter(task => task.visible));
-            } catch (error) {
-                console.error('Ошибка при загрузке данных:', error);
-            }
-        };
-        fetchTasksAndFriends();
-    }, []);
-
-    const mapTaskName = (originalName) => {
-        if (originalName.includes("TG")) {
-            return 'sub to GW telegram';
-        } else if (originalName.includes("twitter")) {
-            return 'sub to Gw x';
-        }
-        return originalName;
-    };
-
     const fetchLevels = async () => {
         try {
             const limitResponse = await axiosInstance.get(`/farm/limit-levels`);
@@ -170,7 +99,7 @@ export default function Page() {
         setSelectedItem(null);
     };
 
-    const handleLimitUpgrade = async (levelId, cost) => {
+    const handleLimitUpgrade = async (levelId) => {
         try {
             const response = await axiosInstance.get(`/farm/limit-level-up?levelId=${levelId}`);
             setLimitLevels(prevLevels => prevLevels.map(item =>
@@ -187,7 +116,7 @@ export default function Page() {
         }
     };
 
-    const handleRateUpgrade = async (levelId, cost) => {
+    const handleRateUpgrade = async (levelId) => {
         try {
             const response = await axiosInstance.get(`/farm/rate-level-up?levelId=${levelId}`);
             setRateLevels(prevLevels => prevLevels.map(item =>
@@ -203,75 +132,6 @@ export default function Page() {
             console.error('Ошибка при улучшении прокачки:', error);
         }
     };
-
-    const openModal = () => {
-        setIsModalOpen(true);
-    };
-    const navigateToPage = (path) => {
-        router.push(path);
-    };
-
-    const timerRef = useRef(null);
-    const handleTaskClick = (task) => {
-        if (task.readyToComplete) {
-            executeTask(task.id);
-            task.readyToComplete = false
-        } else {
-            switch (task.type) {
-                case 1:
-                    navigateToPage(task.path);
-                    break;
-                case 2:
-                    let url = '';
-                    if (task.id === 8) {
-                        url = "https://t.me/gang_wars_game";
-                    } else if (task.id === 9 || task.name && (task.name.toLowerCase().includes("x") || task.name.toLowerCase().includes("twitter"))) {
-                        url = "https://x.com/gangwars_game";
-                    } else {
-                        console.error('URL could not be determined. Task name:', task.name);
-                    }
-                    if (url) {
-                        window.open(url, '_blank');
-                        timerRef.current = setTimeout(() => executeTask(task.id), 500);
-                    }
-                    break;
-                case 3:
-                    navigateToPage(task.path);
-                    break;
-                default:
-                    console.log('No action for this task.');
-            }
-        }
-    };
-    useEffect(() => {
-        return () => {
-            if (timerRef.current) {
-                clearTimeout(timerRef.current);
-            }
-        };
-    }, []);
-    const executeTask = async (taskId) => {
-        try {
-            await axiosInstance.get(`/task/execute?taskId=${taskId}`);
-            fetchCompletedTasks();
-        } catch (error) {
-            console.error(`Error executing task ${taskId}:`, error);
-        }
-    }
-
-    const fetchCompletedTasks = async () => {
-        try {
-            const response = await axiosInstance.get('/task/completed-tasks');
-            const completedTaskIds = response.data.map(task => task.task.id);
-            setTasks(tasks.map(task => ({
-                ...task,
-                completed: completedTaskIds.includes(task.id)
-            })));
-        } catch (error) {
-            console.error('Error fetching completed tasks:', error);
-        }
-    };
-
 
     useEffect(() => {
         if (typeof window !== "undefined") {
@@ -294,19 +154,6 @@ export default function Page() {
         }
     }, [router]);
 
-    const handleTab = (newTab) => {
-        setActiveTab(newTab);
-        router.push({
-            pathname: router.pathname,
-            query: { ...router.query, tab: newTab },
-        });
-    };
-
-    useEffect(() => {
-        if (tab) {
-            setActiveTab(tab);
-        }
-    }, [tab]);
 
     const handleSlideChange = (swiper) => {
         setActiveIndex(swiper.realIndex);
@@ -337,36 +184,16 @@ export default function Page() {
     return (
         <div className={styles.root}>
             <div className={styles.container}>
-                <div className={styles.balance}>{formatNumberFromEnd(coins)}{' '}<Image src={money} alt={''} width={21} height={21} loading="lazy" /></div>
+                <div className={styles.balanceContainer}>
+                    <div className={styles.title}>upgrades</div>
+                    <div className={styles.balance}>{formatNumberFromEnd(coins)}{' '}<Image src={money} alt={''} width={21} height={21} loading="lazy" /></div>
+                </div>
                 <div className={styles.block}>
-                    <div className={styles.buttonSet}>
-                        <div className={styles.folderBtnStats}
-                             style={{
-                                 zIndex: activeTab === '1' ? 112 : 110,
-                                 marginBottom:  activeTab === '1' ? '0px' : '-12px',
-                                 borderRight:  activeTab === '1' ? '2px solid #3842a4' : 'none',
-                             }}
-                             onClick={() => {
-                                 handleTab('1')
-                                 setIsModalOpen(false)
-                             }}>{t('EXP.upgrades')}</div>
-                        <div
-                            className={styles.folderBtnSkins}
-                            style={{
-                                zIndex: activeTab === '2' ? 113 : 110,
-                                marginBottom:  activeTab === '2' ? '-0px' : '2px',
-                            }}
-                            onClick={() => {
-                                handleTab('2')
-                                setIsModalOpen(false)
-                            }}
-                        >{t('EXP.tasks')}</div>
-                    </div>
-                    {activeTab === '1' && <div className={styles.personalContainer}>
+                    <div className={styles.personalContainer}>
                         <div className={styles.list}>
                             <div className={styles.containerSwiper}>
                                 <button className={styles.navLeft} onClick={handleSlidePrev}>
-                                    <Image src={'/Arrow.png'} alt={''} width={15} height={15} loading="lazy" />
+                                    <Image src={'/ArrowWhite.png'} alt={''} width={15} height={15} loading="lazy" />
                                 </button>
                                 <Swiper
                                     modules={[Navigation, Controller]}
@@ -400,7 +227,7 @@ export default function Page() {
                                     ))}
                                 </Swiper>
                                 <button className={styles.navRight} onClick={handleSlideNext}>
-                                    <Image src={'/Arrow.png'} alt={''} width={15} height={15} loading="lazy" />
+                                    <Image src={'/ArrowWhite.png'} alt={''} width={15} height={15} loading="lazy" />
                                 </button>
                             </div>
                             {activeIndex === 0 && <>
@@ -414,35 +241,7 @@ export default function Page() {
                                 ))}</div> : <div className={styles.warning}>{t('EXP.noups')}</div>}
                             </>}
                         </div>
-                    </div>}
-                    {activeTab === '2' && <div className={styles.skinContainer}>
-                        <div className={styles.col}>
-                            {/*<div className={styles.label}>Daily</div>*/}
-                            {/*{Tasks.daily.map((task, index) => {*/}
-                            {/*    return(*/}
-                            {/*        <TaskBtn title={task.name} desc={task.desc} complite={task.complite} key={index} onClick={() => handleTaskClick(task)} />*/}
-                            {/*    )*/}
-                            {/*})}*/}
-                            <div className={styles.label}>{t('EXP.main')}</div>
-                            {tasks.map((task, index) => {
-                                return(
-                                    <>
-                                        {task.type !== 4 && <TaskBtn
-                                            subtitle={task.name}
-                                            desc={task.type !== 2 ? `${task.current} / ${task.amount}` : ''}
-                                            completed={task.completed}
-                                            key={index}
-                                            icon={task.icon}
-                                            type={task.type}
-                                            readyToComplete={task.readyToComplete}
-                                            reward={formatNumberFromEnd(task.reward)}
-                                            onClick={() => handleTaskClick(task)}
-                                        />}
-                                    </>
-                                )
-                            })}
-                        </div>
-                    </div>}
+                    </div>
                 </div>
                 {isUpgradeModalOpen && selectedItem && (
                     <div className={styles.modalOverlay}>
@@ -499,9 +298,6 @@ export default function Page() {
                         </div>
                     </div>
                 )}
-                {isModalOpen && <div className={styles.modal}>
-                    <div className={styles.label}>{t('EXP.close')}</div>
-                </div>}
             </div>
         </div>
     );
