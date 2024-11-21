b5f13b0f57c1ad3e930389439fcb866f3074ba93
Roman Baranovskii <rom8614@gmail.com>
2024-11-21 12:16:49 +0300

    Google Tag Manager

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -103,6 +103,14 @@ export default function App({ Component, pageProps }) {
                     gtag('js', new Date());
                     gtag('config', 'G-QLS2HYFS37');`}
             </Script>
+            <Script id={'Google Tag Manager'} strategy={"afterInteractive"}>
+                {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+                new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+                j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+                'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-NPCQFZWP');`}</Script>
+            {/*<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-NPCQFZWP"*/}
+            {/*                  height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>*/}
             <ToastContainer
                 position="top-center"
                 autoClose={3000}
