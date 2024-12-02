bacadf119886249698cdb5aa7bc357708000594f
Roman Baranovskii <rom8614@gmail.com>
2024-12-03 00:12:52 +0300

    daily clan popup

--- src/pages/main/index.js
+++ src/pages/main/index.js
@@ -12,8 +12,8 @@ import teamData from "@/mock/teamsData.js";
 import skinData from '@/mock/skinsData'
 
 import styles from "@/styles/Home.module.scss";
-import {use} from "i18next";
 import {formatNumber} from "@/utils/formatNumber";
+import axios from "@/utils/axios";
 
 const account = '/main-buttons/account.png'
 const settings = '/main-buttons/settings.png'
@@ -24,6 +24,7 @@ const claimClicked = '/lootBTNclicked.png'
 const border = '/totalbar.png'
 const background = '/backgrounds/nightcity.png'
 const bonus = '/main-buttons/bonus.png'
+const money = '/money.png'
 
 export default function Home() {
     const router = useRouter();
@@ -34,6 +35,8 @@ export default function Home() {
     const [startFarmTime, setStartFarmTime] = useState(Date.now());
     const [isClaimClicked, setIsClaimClicked] = useState(false);
     const [gameBonus, setGameBonus] = useState(false)
+    const [clanId, setClanId] = useState(null)
+    const [clanPopUp, setClanPopUp] = useState(false)
 
     const { collectAndStart } = useFarmCollect();
 
@@ -57,6 +60,23 @@ export default function Home() {
             }
         }
     }, [balance, startFarmTime, isClaimClicked, coins]);
+
+    useEffect(() => {
+        const lastClan = localStorage.getItem('dailyClan');
+        const today = new Date().toISOString().split('T')[0];
+        if (lastClan !== today) {
+            axios.get('/group/last')
+                .then(response => {
+                    const { groupId } = response.data;
+                    setClanId(groupId);
+                    setClanPopUp(true);
+                    localStorage.setItem('lastFetchDate', today);
+                })
+                .catch(error => {
+                    console.error('Ошибка при выполнении запроса:', error);
+                });
+        }
+    }, []);
 
     useEffect(() => {
         if (typeof window !== "undefined") {
@@ -130,48 +150,67 @@ export default function Home() {
     const maxWidth = 224;
     const currentWidth = (currentFarmCoins / limit) * maxWidth;
 
+    const closePopUp = () => {
+        setClanPopUp(false)
+    }
+
     return (
-        <div className={styles.root}>
-            <Image className={styles.background} src={background} width={300} height={1000}  alt={'bg'} loading="lazy"/>
-            <div className={styles.item1}>
-                <IconButton image={account} alt={'account'} title={t('main.account')}  onClick={() => {router.push('/account')}}/>
+        <>
+            <div className={styles.root}>
+                <Image className={styles.background} src={background} width={300} height={1000}  alt={'bg'} loading="lazy"/>
+                <div className={styles.item1}>
+                    <IconButton image={account} alt={'account'} title={t('main.account')}  onClick={() => {router.push('/account')}}/>
+                </div>
+                <div className={styles.item2}>
+                    <IconButton image={teamData[groupId]?.logo} alt={'gang'} onClick={() => {router.push('/change')}}/>
+                </div>
+                <div className={styles.item3}>
+                    <IconButton image={settings} alt={'settings'} title={t('main.settings')} onClick={() => {router.push('/settings');}}/>
+                </div>
+                <div className={styles.item4}>
+                    <IconButton image={boards} alt={'boards'} title={t('main.boards')} onClick={() => {router.push('/boards');}}/>
+                </div>
+                <div className={styles.item5}>
+                    <Image src={border} width={600} height={200} alt={'border'} className={styles.totalBarRoot} loading="lazy"/>
+                    <div className={styles.totalText}>{formatNumber(balance, 12)}</div>
+                </div>
+                <div className={styles.item6}>
+                    <IconButton image={wallet} alt={'wallet'} title={t('main.wallet')} hidden={true} onClick={() => {router.push('/main')}}/>
+                </div>
+                <div className={styles.item7}>
+                    <Image width={1000} height={1000} className={styles.char} alt={'character'} src={skinData[groupId]?.[liga]?.icon} loading="lazy"/>
+                </div>
+                {gameBonus && <div className={styles.bonusItem}>
+                    <Image src={bonus} alt={''} width={60} height={60}/>
+                </div>}
+                <div className={styles.item8}>
+                    <CollectBar
+                        currentCoins={formatNumberFromEnd(currentFarmCoins < 0 ? 0 : currentFarmCoins)}
+                        maxCoins={formatNumber(Number(limit), 6)}
+                        width={currentWidth}
+                    />
+                </div>
+                <div className={styles.item9} id={"loot"} onClick={handleClaimClick}>
+                    <Image id={"loot"} className={styles.claimRoot} width={600} height={200} src={isClaimClicked ? claimClicked : claim} alt={'claim'} loading="lazy" />
+                    <p id={"loot"} className={isClaimClicked ? styles.btnClicked : styles.btn}>{t('main.loot')}</p>
+                </div>
+                <div className={styles.item10}>
+                    <NavBar/>
+                </div>
             </div>
-            <div className={styles.item2}>
-                <IconButton image={teamData[groupId]?.logo} alt={'gang'} onClick={() => {router.push('/change')}}/>
-            </div>
-            <div className={styles.item3}>
-                <IconButton image={settings} alt={'settings'} title={t('main.settings')} onClick={() => {router.push('/settings');}}/>
-            </div>
-            <div className={styles.item4}>
-                <IconButton image={boards} alt={'boards'} title={t('main.boards')} onClick={() => {router.push('/boards');}}/>
-            </div>
-            <div className={styles.item5}>
-                <Image src={border} width={600} height={200} alt={'border'} className={styles.totalBarRoot} loading="lazy"/>
-                <div className={styles.totalText}>{formatNumber(balance, 12)}</div>
-            </div>
-            <div className={styles.item6}>
-                <IconButton image={wallet} alt={'wallet'} title={t('main.wallet')} hidden={true} onClick={() => {router.push('/main')}}/>
-            </div>
-            <div className={styles.item7}>
-                <Image width={1000} height={1000} className={styles.char} alt={'character'} src={skinData[groupId]?.[liga]?.icon} loading="lazy"/>
-            </div>
-            {gameBonus && <div className={styles.bonusItem}>
-                <Image src={bonus} alt={''} width={60} height={60}/>
+            {clanPopUp && <div className={styles.clanPopUp}>
+                <div className={styles.popUpClose} onClick={closePopUp}>x</div>
+                <div className={styles.clanBorder}>
+                    <div className={styles.clanContainer}>
+                        <div className={styles.clanLabel}>Daily Pvp<br/> results  </div>
+                        <div className={styles.clanName}>{teamData[clanId]?.Name}</div>
+                        <Image src={teamData[clanId]?.logo} alt={''} width={120} height={120} />
+                        <div className={styles.clanLabel}>Are <a>oG</a>s TODAY</div>
+                        <div className={styles.clanName}>+50000 <Image src={money} alt="" width={15} height={15} /></div>
+                    </div>
+                </div>
             </div>}
-            <div className={styles.item8}>
-                <CollectBar
-                    currentCoins={formatNumberFromEnd(currentFarmCoins < 0 ? 0 : currentFarmCoins)}
-                    maxCoins={formatNumber(Number(limit), 6)}
-                    width={currentWidth}
-                />
-            </div>
-            <div className={styles.item9} id={"loot"} onClick={handleClaimClick}>
-                <Image id={"loot"} className={styles.claimRoot} width={600} height={200} src={isClaimClicked ? claimClicked : claim} alt={'claim'} loading="lazy" />
-                <p id={"loot"} className={isClaimClicked ? styles.btnClicked : styles.btn}>{t('main.loot')}</p>
-            </div>
-            <div className={styles.item10}>
-                <NavBar/>
-            </div>
-        </div>
+        </>
+
     );
 }
