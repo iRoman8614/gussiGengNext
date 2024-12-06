4b628d4197d3f92a5c6d518523d03c0b8802bd4f
Roman Baranovskii <rom8614@gmail.com>
2024-12-07 01:28:34 +0300

    daily popup

--- src/pages/main/index.js
+++ src/pages/main/index.js
@@ -151,6 +151,7 @@ export default function Home() {
         if (!dailyTaskDate || dailyTaskDate !== today) {
             localStorage.setItem("dailyTaskDate", today);
             loadAndExecuteTasks();
+            setDailyPopUp(true);
         }
     }, [dailyEntries]);
 
@@ -181,7 +182,6 @@ export default function Home() {
                 .map((item) => item.task.id);
             setCompletedTaskIds(updatedCompletedTaskIds);
             console.log("Все недостающие задания выполнены.");
-            setDailyPopUp(true);
         } catch (error) {
             console.error("Ошибка при загрузке или выполнении задач:", error);
         }
