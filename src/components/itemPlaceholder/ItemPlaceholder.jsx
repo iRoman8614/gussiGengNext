685ecbe99fcf78de6d0a47d5e5ce50c69d03a18e
Roman Baranovskii <rom8614@gmail.com>
2024-12-14 23:59:54 +0300

    faq x + locks

--- src/components/itemPlaceholder/ItemPlaceholder.jsx
+++ src/components/itemPlaceholder/ItemPlaceholder.jsx
@@ -4,6 +4,7 @@ import styles from './ItemPlaceholder.module.scss';
 import {formatNumber} from "@/utils/formatNumber";
 
 const money = '/money.png'
+const Lock = '/Lock.png'
 
 // eslint-disable-next-line react/prop-types
 export const ItemPlaceholder = ({ item, img, onClick, available }) => {
@@ -22,6 +23,11 @@ export const ItemPlaceholder = ({ item, img, onClick, available }) => {
                 <Image className={styles.image} width={260} height={170} alt="" src={img} priority />
                 <div className={styles.level}>lvl {item.level}</div>
                 <div className={styles.per}>+{item.increasePer}%</div>
+                {!available && <Image className={styles.lock} width={70} height={70} alt="" src={Lock} priority/>}
+                {!available && <div className={styles.lock}>
+                    <Image className={styles.lock} width={70} height={70} alt="" src={Lock} priority/>
+                    <div className={styles.level}>Requires lvl 10 of the prev card</div>
+                </div>}
             </div>
             <div className={styles.title}>
                 {formatNumber(Number(item.cost))}{' '}
